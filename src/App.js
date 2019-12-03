91576105894ce5d0d51100af0de7cb14017e1472
Aurélien Girault <me@agirault.com>
2019-12-03 18:34:06 +0100

    chore(style): use scss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 import { Provider } from 'react-redux'
-import './App.css'
+import './App.scss'
 import Blog from './pages/Blog'
 
 class App extends Component {
