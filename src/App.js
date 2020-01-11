b7631d9dfd669827f44d797ffb2cd7dd1b8f227c
Aurélien Girault <me@agirault.com>
2020-01-11 11:10:22 +0100

    chore(style): improvements

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 import { Provider } from 'react-redux'
-import './App.scss'
-import Blog from './pages/Blog'
+import Blog from './components/Blog'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Provider store={store}>
-          <BrowserRouter>
-            <Blog></Blog>
-          </BrowserRouter>
-        </Provider>
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Blog></Blog>
+        </BrowserRouter>
+      </Provider>
     )
   }
 }
